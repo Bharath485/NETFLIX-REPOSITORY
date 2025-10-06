b8ee20cf1dc4f78bc9bebfbe5bfb435cb6a2ce97
K Bharath Reddy <kbharath.10731399@ltimindtree.com>
2025-10-06 20:56:35 +0530

    Auto commit from Flask app triggered by C:\Users\10731399\Downloads\GitPOC\cloned_repo\models\fct\fct_enriched_movie_insights_4.sql

--- models/fct/fct_enriched_movie_insights_4.sql
+++ models/fct/fct_enriched_movie_insights_4.sql
@@ -1,11 +1,34 @@
-{{ config(
-    materialized='view'
-) }}
+### Generated DBT Code ###
 
-with current_time_data as (
+-- This model calculates enriched movie insights
+
+WITH base_data AS (
     SELECT
-        CURRENT_TIMESTAMP() AS current_time,
-        'Bharath' AS Bharath
+        movie_id,
+        title,
+        release_year,
+        genre,
+        rating
+    FROM {{ ref('stg_movies') }}
+),
+
+aggregated_data AS (
+    SELECT
+        genre,
+        AVG(rating) AS avg_rating,
+        COUNT(movie_id) AS total_movies
+    FROM base_data
+    GROUP BY genre
 )
 
-SELECT * FROM current_time_data
+SELECT
+    bd.movie_id,
+    bd.title,
+    bd.release_year,
+    bd.genre,
+    bd.rating,
+    ad.avg_rating,
+    ad.total_movies
+FROM base_data bd
+LEFT JOIN aggregated_data ad
+ON bd.genre = ad.genre
