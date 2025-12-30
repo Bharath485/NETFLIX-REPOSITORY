123a086d041fd7a1a8612ddc28aa06bda6b23e3a
bharath485 <bharath4507@gmail.com>
2025-12-30 16:12:59 +0530

    New Message

--- models/fct/models/marts/fact_inventory.sql
+++ models/fct/models/marts/fact_inventory.sql
@@ -1,44 +1,32 @@
 {{ config(materialized='table') }}
 
-with source_data as (
+with base as (
     select
-        snapshot_date,
-        store_code,
-        product_code,
-        on_hand_qty,
-        on_order_qty
-    from {{ source('staging', 'stg_inventory') }}
-),
-
-store_lookup as (
-    select
-        store_code,
-        store_key
-    from {{ ref('dim_store') }}
-),
-
-product_lookup as (
-    select
-        product_code,
-        product_key
-    from {{ ref('dim_product') }}
-),
-
-final as (
-    select
-        cast(to_char(current_date(), 'YYYYMMDD') as varchar) as date_key,
-        cast(s.snapshot_date as date) as dt_of_snapshot,
-        coalesce(st.store_key, -1) as store_key,
-        coalesce(p.product_key, -1) as product_key,
-        cast(coalesce(s.on_hand_qty, 0) as integer) as on_hand_qty,
-        cast(coalesce(s.on_order_qty, 0) as integer) as on_order_qty,
-        cast(current_timestamp() as timestamp) as dt_created,
-        cast(current_timestamp() as timestamp) as dt_modified
-    from source_data s
-    left join store_lookup st
-        on s.store_code = st.store_code
-    left join product_lookup p
-        on s.product_code = p.product_code
+        -- Date key in YYYYMMDD format
+        cast(to_char(snapshot_date, 'YYYYMMDD') as integer) as date_key,
+        
+        -- Snapshot date
+        cast(snapshot_date as date) as dt_of_snapshot,
+        
+        -- Store key lookup
+        ds.store_key,
+        
+        -- Product key lookup  
+        dp.product_key,
+        
+        -- Inventory quantities with default 0
+        coalesce(cast(on_hand_qty as integer), 0) as on_hand_qty,
+        coalesce(cast(on_order_qty as integer), 0) as on_order_qty,
+        
+        -- Audit timestamps
+        current_timestamp as dt_created,
+        current_timestamp as dt_modified
+        
+    from {{ ref('stg_inventory') }} si
+    left join {{ ref('dim_store') }} ds 
+        on si.store_code = ds.store_code
+    left join {{ ref('dim_product') }} dp 
+        on si.product_code = dp.product_code
 )
 
-select * from final
+select * from base
