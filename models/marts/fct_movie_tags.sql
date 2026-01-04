cd4f1413c992b44fe45acb8bd408a19459fc9e89
bharath485 <bharath4507@gmail.com>
2026-01-04 22:28:49 +0530

    Fact Inventory Table

--- models/marts/fct_movie_tags.sql
+++ models/marts/fct_movie_tags.sql
@@ -1,8 +1,7 @@
 {{ config(materialized="table") }}
 
 select
-    cast(movieId as integer) as movie_id,
-    cast(tagId as integer) as tag_id,
-    cast(from_unixtime(timestamp) as date) as tag_date
-from {{ source('raw', 'r_movie_tags') }}
-;
+    cast(movieid as integer) as movie_id,
+    cast(tagid as integer) as tag_id,
+    cast(to_timestamp(timestamp) as date) as tag_date
+from {{ source('netflix', 'r_movie_tags') }}
