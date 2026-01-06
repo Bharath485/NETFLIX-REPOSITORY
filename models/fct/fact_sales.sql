a61f7f1c5c1f5ebdbd3945cb433f7ac2d7a1a878
bharath485 <bharath4507@gmail.com>
2026-01-07 02:56:43 +0530

    Fact Inventory Table

--- models/fct/fact_sales.sql
+++ models/fct/fact_sales.sql
@@ -5,8 +5,8 @@ with source_data as (
         sale_date,
         store_code,
         product_code,
-        coalesce(sale_qty, 0) as sale_qty
-    from {{ ref('stg_sales') }}
+        sale_qty
+    from {{ source('staging', 'stg_sales') }}
 ),
 
 store_lookup as (
@@ -28,9 +28,9 @@ final as (
         cast(to_char(s.sale_date, 'YYYYMMDD') as integer) as sale_date_key,
         coalesce(st.store_key, -1) as store_key,
         coalesce(p.product_key, -1) as product_key,
-        cast(s.sale_qty as integer) as sale_qty,
-        current_timestamp() as dt_created,
-        current_timestamp() as dt_modified
+        cast(coalesce(s.sale_qty, 0) as integer) as sale_qty,
+        cast(current_timestamp() as timestamp) as dt_created,
+        cast(current_timestamp() as timestamp) as dt_modified
     from source_data s
     left join store_lookup st
         on s.store_code = st.store_code
